4219ae667d1ece5666b82f350c825e35e1f1f1d1
INZDY <57595839+INZDY@users.noreply.github.com>
2024-05-26 23:34:16 +0700

    style: code formatting and some display changes.

--- Frontend/src/components/FilterSwitch.tsx
+++ Frontend/src/components/FilterSwitch.tsx
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
 import { FilterToggleSwitchProps } from "../interfaces/filterSwitch";
 
-
-
-const FilterToggleSwitch: React.FC<FilterToggleSwitchProps> = ({ onFilterChange }) => {
+const FilterToggleSwitch: React.FC<FilterToggleSwitchProps> = ({
+  onFilterChange,
+}) => {
   const [filter, setFilter] = useState<"all" | "completed" | "pending">("all");
 
   const handleChange = () => {
-    const nextFilter = filter === "all" ? "completed" : filter === "completed" ? "pending" : "all";
+    const nextFilter =
+      filter === "all"
+        ? "completed"
+        : filter === "completed"
+        ? "pending"
+        : "all";
     setFilter(nextFilter);
     onFilterChange(nextFilter);
   };
 
   return (
-    <div className="filter-toggle-switch">
-      <select 
-      value={filter} 
-      onChange={handleChange}  
-      className="rounded-full bg-white text-black px-4 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    <div className="">
+      <span className="font-semibold text-lg text-white mr-4">Filter: </span>
+      <select
+        value={filter}
+        onChange={handleChange}
+        className="rounded-md bg-white text-black p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
-        <option value="all">Show All</option>
-        <option value="completed">Show Completed</option>
-        <option value="pending">Show Pending</option>
+        <option value="all">All</option>
+        <option value="completed">Completed</option>
+        <option value="pending">Pending</option>
       </select>
     </div>
   );
 };
 
 export default FilterToggleSwitch;
-
