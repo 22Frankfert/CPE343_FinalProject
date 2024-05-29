22098b74c37bf5f46147bac1857ed649eacc7999
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-05-29 18:36:17 +0700

    refactor: separate bussiness login from service

--- Backend/src/todo/todo.service.prisma.ts
+++ Backend/src/todo/todo.service.prisma.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -12,9 +12,6 @@ export class PrismaRepository extends BaseService<Item> {
   }
 
   create(createTodoDto: CreateTodoDto): Promise<Item> {
-    if (createTodoDto.dueDate)
-      createTodoDto.dueDate = new Date(createTodoDto.dueDate);
-
     return this.prismaService.item.create({
       data: createTodoDto,
     });
@@ -31,8 +28,6 @@ export class PrismaRepository extends BaseService<Item> {
   }
 
   update(id: string, updateTodoDto: UpdateTodoDto): Promise<Item> {
-    if (updateTodoDto.dueDate)
-      updateTodoDto.dueDate = new Date(updateTodoDto.dueDate);
 
     return this.prismaService.item.update({
       where: { id },
@@ -41,10 +36,6 @@ export class PrismaRepository extends BaseService<Item> {
   }
 
   async remove(id: string): Promise<Item> {
-    const exitingItem = await this.prismaService.item.findUnique({
-      where: { id },
-    });
-    if (!exitingItem) throw new NotFoundException('Item not found');
 
     return this.prismaService.item.delete({
       where: { id },
--- Backend/src/todo/todo.service.ts
+++ Backend/src/todo/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Item } from '@prisma/client';
@@ -9,6 +9,10 @@ export class TodoService {
   constructor(private readonly baseService: BaseService<Item>) {}
 
   create(createTodoDto: CreateTodoDto): Promise<Item> {
+
+    if (createTodoDto.dueDate)
+      createTodoDto.dueDate = new Date(createTodoDto.dueDate);
+
     return this.baseService.create(createTodoDto);
   }
 
@@ -21,10 +25,18 @@ export class TodoService {
   }
 
   update(id: string, updateTodoDto: UpdateTodoDto): Promise<Item> {
+
+    if (updateTodoDto.dueDate)
+      updateTodoDto.dueDate = new Date(updateTodoDto.dueDate);
+
     return this.baseService.update(id, updateTodoDto);
   }
 
-  remove(id: string): Promise<Item> {
+  async remove(id: string): Promise<Item> {
+
+    const exitingItem = await this.baseService.findOne(id);
+    if (!exitingItem) throw new NotFoundException('Item not found');
+
     return this.baseService.remove(id);
   }
 }
